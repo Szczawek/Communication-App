e7ab9447603c7560534e487702b1c0597332d087
Szczawik <szczawik.rozwoju@wp.pl>
2024-05-09 11:56:59 +0200

    'https' react and server

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import "dotenv/config";
+ds
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    https: {
+      key: process.env.KEY_SSH,
+      cert: process.env.CERT_SSH,
+    },
+  },
+});
--- server/server.js
+++ server/server.js
@@ -1,1 +1,36 @@
-ECHO is on.
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import https from "https";
+import helmet from "helmet";
+const PORT = 443;
+const app = express();
+
+app.use(cors());
+app.use(express());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "default-src": ["'self'"],
+        "img-src": ["'self'", "blob:"],
+        "script-src": ["'self'"],
+      },
+    },
+  })
+);
+
+const options = {
+  key: process.env.KEY_SSH,
+  cert: process.env.CERT_SSH,
+};
+const server = https.createServer(options, app);
+// START POINT
+app.get("/", (req, res) => {
+  res.send("Szczawik");
+});
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`https://localhost:${PORT}`);
+});
